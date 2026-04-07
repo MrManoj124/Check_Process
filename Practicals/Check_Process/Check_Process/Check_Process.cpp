c2a8796194fb1f188618e8dc3a173ba0bd9cbdc9
MrManoj124 <manojmanorooban@gmail.com>
2026-04-08 00:40:42 +0530

    Create a basic code for get pid and Processes

--- Practicals/Check_Process/Check_Process/Check_Process.cpp
+++ Practicals/Check_Process/Check_Process/Check_Process.cpp
@@ -1,44 +1,17 @@
 #include <mpi.h>
 #include <stdio.h>
+
 int main()
 {
 	int np;
 	int pid;
+
 	MPI_Init(NULL, NULL);
-	MPI_Comm_size(MPI_COMM_WORLD, &np);
-	MPI_Comm_rank(MPI_COMM_WORLD, &pid);
-	//printf("\n Hello world . . . \n My pid= %d \n No_of_process= %d", pid, np);
+	MPI_Comm_Size(MPI_COMM_WORLD, &np);
+	MPI_Comm_Rank(MPI_COMM_WORLD, &pid);
 
-	/*if (pid == 0)
-	{
-		printf("\n Helllo World.... \n My Pid = %d \n  No_of_Process = %d", pid, np);
-	}
-	else if (pid == 1)
-	{
-		printf("\n Mr Manorooban \n My Pid = %d \n  No_of_Process = %d", pid, np);
-	}
-	else if (pid == 2)
-	{
-		printf("\n Start to Study Parallel \n My Pid = %d \n  No_of_Process = %d", pid, np);
-	}
-	else
-	{
-		printf("\n Computing. . . \n my pid= %d \n no_of_process= %d", pid, np);
-	}
-	*/
+	printf("\n Mr Manorooban.... \n My pid = %d  \n No_Of_Process=%d, pid, np");
 
-	if (pid == 0)
-	{
-		int num = 400;
-		MPI_Send(&num, 1, MPI_INT, 1, 50, MPI_COMM_WORLD);
-		printf("\n Hello world 1 . . . \n My pid= %d \n No_of_process= %d", pid, np);
-	}
-	else
-	{
-		int rec_num;
-		MPI_Recv(&rec_num, 1, MPI_INT, 0, 50, MPI_COMM_WORLD, &sta);
-		printf("\n Hello Manorooban.... \n My pid = %d \n Received Number is %d", pid, np, rec_num);
-	}
 	MPI_Finalize();
 	return 0;
 }
--- Practicals/Check_Process/Check_Process/OneToMany.cpp
+++ Practicals/Check_Process/Check_Process/OneToMany.cpp
@@ -1,27 +1,13 @@
-#include <mpi.h> 
-#include <stdio.h> 
+#include <mpi.h>
+#include <stdio.h>
 int main()
 {
 	int np;
 	int pid;
 	MPI_Init(NULL, NULL);
-	MPI_Comm_size(MPI_COMM_WORLD, &np);
-	MPI_Comm_rank(MPI_COMM_WORLD, &pid);
-
-
-	if (pid == 0)
-	{
-		int num = 400;
-		MPI_Send(&num, 1, MPI_INT, 1, 50, MPI_COMM_WORLD);
-
-		printf("\n Hello world 1 . . . \n My pid= %d \n No_of_process= %d", pid, np);
-	}
-	else
-	{
-		int rec_num;
-		MPI_Recv(&rec_num,  1, MPI_INT, 0, 50,MPI_COMM_WORLD, &sta);
-		printf("\n Hello world 2. . . \n My pid= %d \n No_of_process= %d \n Recieved Number is %d", pid, np, rec_num);
-	}
+	MPI_Comm_Size(MPI_COMM_WORLD, &np);
+	MPI_Comm_Rank(MPI_COMM_WORLD, &pid);
+	printf("\n My name is Mr.Manorooban \n My pid is=%d, N0_Of _Process is =%d , pid, np");
 
 	MPI_Finalize();
 	return 0;
