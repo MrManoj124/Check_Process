31f992bfc735ab74a02beab39eda806c45d588fe
MrManoj124 <manojmanorooban@gmail.com>
2026-04-08 20:15:16 +0530

    create a  another simple file

--- Practicals/Check_Process/Check_Process/Check_Process.cpp
+++ Practicals/Check_Process/Check_Process/Check_Process.cpp
@@ -1,4 +1,4 @@
-#include <mpi.h>
+/*#include <mpi.h>
 #include <stdio.h>
 
 int main()
@@ -29,6 +29,23 @@ int main()
 
 		printf("\n crack it with MPI  \n From = %d \n No_Of_Process is = %d", pid, np);
 	}
+
+	MPI_Finalize();
+	return 0;
+}
+*/
+
+#include <mpi.h>
+#include <stdio.h>
+int main() {
+	int pid;
+	int np;
+
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &np);
+	MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+
+	printf("\n Hey Buddy... \n MyPid=%d \n No_Of_Process=%d", pid, np);
 
 	MPI_Finalize();
 	return 0;
