e48aef31110dad127daf0be2c1aedfdc2946115f
MrManoj124 <manojmanorooban@gmail.com>
2026-04-09 01:44:39 +0530

    check the master

--- Practicals/Check_Process/Check_Process/Check_Process.cpp
+++ Practicals/Check_Process/Check_Process/Check_Process.cpp
@@ -86,3 +86,21 @@ int main() {
 
 
 //Start to practice with OnetoMany Operation
+#include <mpi.h>
+#include <stdio.h>
+
+int main() {
+	int np;
+	int pid;
+
+	MPI_Init(NULL, NULL);
+	MPI_Comm_size(MPI_COMM_WORLD, &np);
+	MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+
+	if (pid == 0)
+	{
+		int num = 3;
+
+	}
+
+}
