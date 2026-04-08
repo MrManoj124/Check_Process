c1d3ea544380ffb1d1410a35e703c811b56c2991
MrManoj124 <manojmanorooban@gmail.com>
2026-04-08 20:01:52 +0530

    check the master with pid==0

--- Practicals/Check_Process/Check_Process/Check_Process.cpp
+++ Practicals/Check_Process/Check_Process/Check_Process.cpp
@@ -10,7 +10,13 @@ int main()
 	MPI_Comm_size(MPI_COMM_WORLD, &np);
 	MPI_Comm_rank(MPI_COMM_WORLD, &pid);
 
-	printf("\n Mr Manorooban.... \n My pid = %d  \n No_Of_Process=%d", pid, np );
+	//printf("\n Mr Manorooban.... \n My pid = %d  \n No_Of_Process=%d", pid, np );
+
+	if (pid == 0)
+	{
+		printf("\n Hey Guys let's study Parallal Computing  \n From = %d \n No_Of_Process is = %d", pid, np);
+	}
+	
 
 	MPI_Finalize();
 	return 0;
