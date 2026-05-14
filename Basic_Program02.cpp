21815a65a255383e6d38c9ab6185692078fe559e
MrManoj124 <manojmanorooban@gmail.com>
2026-05-14 17:38:50 +0530

    Add basic MPI setup for the file

---
+++ Basic_Program02.cpp
@@ -0,0 +1,14 @@
+#include<mpi.h>
+#include<stdio.h>
+
+int main(){
+    int np, pid;
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &np);
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+    MPI_Status sta;
+
+    
+    MPI_finalize();
+    return 0;
+}
