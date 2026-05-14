b67a4119496f25335627ed392060cd94fcad109e
MrManoj124 <manojmanorooban@gmail.com>
2026-05-14 18:04:49 +0530

    create nested for loop for check prime numbers

--- Basic_Program02.cpp
+++ Basic_Program02.cpp
@@ -13,7 +13,15 @@ int main(){
     if(pid == 0){
         int Prime_numbers[10];
         int count = 0;
-        
+        for(int i=2; count<10; i++){
+            bool isPrime = true;
+            for(int j=2; j<=i/2; j++){
+                if(i % j == 0){
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
     }
     
     }
