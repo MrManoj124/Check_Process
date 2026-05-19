88724a1aa702f630d098c7edba34f8e14d9e4777
MrManoj124 <manojmanorooban@gmail.com>
2026-05-19 17:39:15 +0530

    create an array inside the master process and send it Using MPI_Send

--- Basic_program03.cpp
+++ Basic_program03.cpp
@@ -6,3 +6,16 @@ pid 1 should receive the values and find the sum
 #include<mpi.h>
 #include<stdio.h>
 #include<string.h>
+int main(){
+    int np;
+    int pid;
+    MPI_Init(NULL, NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &np);
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+    MPI_Status sta;
+    if(pid == 0){
+        int Send_Num[5] = {10,20,30,40,50};
+        MPI_Send(Send_Num, 5, MPI_INT, 1, 50, MPI_COMM_WORLD);
+        printf("My processor id is %d and I'm the sender. \n", pid);
+      }
+}
