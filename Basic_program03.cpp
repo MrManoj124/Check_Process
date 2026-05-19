3b53a3cf67e294b6278ccb741ea70e9b1001b5cd
MrManoj124 <manojmanorooban@gmail.com>
2026-05-19 17:34:53 +0530

    Add all the libraries

---
+++ Basic_program03.cpp
@@ -0,0 +1,8 @@
+// Program 03 Execution
+/* PID 0 has an array of integer values, It should send the array to pid 1 and 
+pid 1 should receive the values and find the sum
+*/
+
+#include<mpi.h>
+#include<stdio.h>
+#include<string.h>
