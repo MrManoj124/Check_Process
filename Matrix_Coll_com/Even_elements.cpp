b67cd4828c57998bd3ea406a939c1451d6450a9e
MrManoj124 <manojmanorooban@gmail.com>
2026-05-25 23:12:13 +0530

    Add a for loop to count the number of even elements

--- Matrix_Coll_com/Even_elements.cpp
+++ Matrix_Coll_com/Even_elements.cpp
@@ -10,13 +10,41 @@ int main(){
     MPI_Comm_size(MPI_COMM_WORLD, &np);
     MPI_Comm_Rank(MPI_COMM_WORLD, &pid);
 
-    // Initializes the array in master process
+     /*
+    2 5 8 11
+    14 17 20 23
+    26 29 32 35
+    38 41 44 47
+    */
+   int RowA[4][4];
+
+   int RowB[4];
+   int localCount = 0;
+
+   int gatheredcount[4];
+   int totaleven = 0;
+
+    // a. Processor 0 initializes the following matrix A of size 4 × 4
     if(pid == 0){
         int Arr[4][4]={{2, 5, 8, 11},
                         {14, 17, 20, 23},
                         {26, 29, 32, 35},
                         {38, 41, 44, 47}};
 
+   // b. Distribute rows among processors using MPI_Scatter
+        MPI_Scatter(Arr, 4, MPI_INT, RowA, 4, MPI_INT, 0, MPI_COMM_WORLD);
+
+
+
+   // c. Each processor should count the number of even elements in its
+       // row.
+        for(int i=0; i<4; i++){
+            if(RowA[i] % 2 == 0){
+                localCount++;
+            }
+        }
+
+   
     MPI_Finalize();
     return 0;
 }
