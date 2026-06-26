1a4af9799e0d6ff6827847b488a053cdf9c7ad24
MrManoj124 <manojmanorooban@gmail.com>
2026-06-26 21:41:36 +0530

    initialise the basic variables for the process

--- Tutorial01.cpp
+++ Tutorial01.cpp
@@ -7,6 +7,18 @@ int main(){
     MPI_Comm_size(MPI_COMM_WORLD, &np);
     MPI_Comm_rank(MPI_COMM_WORLD, &pid);
 
+    int data[n];
+    int local_data[n];
+    int local_sum=0;
+
+    int local_n = n/np;
+
+    // 1. The program is run with P processes (P ≥ 2).
+    if(pid == 0){
+        printf("Hey Mano Let's Break it the whole Structure of Parallalism ...., Process id is  : %d \n ", pid);
+    }
+
+    // 2. Only process 0 (the root) creates a large 1D array of integers called data of size N, where N is perfectly divisible by P (N = 400)
     if(pid == 0){
         printf("Hey Mano Let's Break it the whole Structure of Parallalism ...., Process id is  : %d \n ", pid);
     }
