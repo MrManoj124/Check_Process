55547b955c37ab16013db6e4009d3453d81ed83c
MrManoj124 <manojmanorooban@gmail.com>
2026-06-26 21:44:27 +0530

    create a for loop for initialise the data

--- Sumof400.cpp
+++ Sumof400.cpp
@@ -15,7 +15,15 @@ int main(){
 
     // 1. The program is run with P processes (P ≥ 2).
     if(pid == 0){
-        printf("Hey Mano Let's Break it the whole Structure of Parallalism ...., Process id is  : %d \n ", pid);
+        // printf("Hey Mano Let's Break it the whole Structure of Parallalism ...., Process id is  : %d \n ", pid);
+   
+        // Initialise the data 
+        for(int i=0; i<n; i++){
+            data[i]=i+1;
+        } 
+
+        // send chunks to other process
+        
     }
 
     // 2. Only process 0 (the root) creates a large 1D array of integers called data of size N, where N is perfectly divisible by P (N = 400)
