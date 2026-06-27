40ef446fe2a57328ea9ab3834835e6eaca518c3a
MrManoj124 <manojmanorooban@gmail.com>
2026-06-27 07:09:54 +0530

    feat : complete the code for ring topology

--- T2Ring.cpp
+++ T2Ring.cpp
@@ -9,8 +9,19 @@ int main(){
     MPI_Comm_size(MPI_COMM_WORLD, &np);
     MPI_Comm_rank(MPI_COMM_WORLD, &pid);
 
-    if(pid == 0){
+    MPI_Status sta;
+    int value;
 
+    if(pid == 0){
+        value = 100 ;
+        MPI_Send(&value, 1, MPI_INT, 0, 50, MPI_COMM_WORLD);
+        MPI_Recv(&value, 1, MPI_INT, np-1, 50, MPI_COMM_WORLD, &sta);
+        printf("Process %d Received final value : %d \n ", pid, value);
+    }
+    else{
+        MPI_Recv(&value, 1, MPI_INT, pid-1, 50, MPI_COMM_WORLD, &sta);
+        prinf("Process %d received value : %d \n", pid, value);
+        MPI_Send(&value, 1, MPI_INT, (pid+1)%np, 50, MPI_COMM_WORLD);
     }
 
 
