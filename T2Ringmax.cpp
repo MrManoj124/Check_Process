6136a3ddddb2c9564b00c484e2c551b37b2088f7
MrManoj124 <manojmanorooban@gmail.com>
2026-06-27 07:19:52 +0530

    Check the master process and send the sum to slave process

---
+++ T2Ringmax.cpp
@@ -0,0 +1,24 @@
+#include<mpi.h>
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    int np,pid;
+
+    MPI_Comm_Init(NULL,NULL);
+    MPI_Comm_size(MPI_COMM_WORLD, &np);
+    MPI_Comm_rank(MPI_COMM_WORLD, &pid);
+
+    MPI_Status sta;
+    int sum;
+
+    if(pid == 0){
+        sum = 0;
+        MPI_Send(&sum, 1, MPI_INT, 1, 20, MPI_COMM_WORLD);
+        MPI_Recv(&sum, 1, MPI_INT, np-1, 20, MPI_COMM_WORLD, &sta);
+        printf("Process %d Final Ring sum %d \n", pid, sum);
+    }
+    
+
+    MPI_Finalize();
+}
